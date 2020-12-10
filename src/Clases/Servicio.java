6339bda527a2adc75ef16cd0c930a33a54d92cd2
Juan David Arango Pulido <jd.arangop@uniandes.edu.co>
2020-12-09 22:13:37 -0500

    Implementacion libraria java.time

--- src/Clases/Servicio.java
+++ src/Clases/Servicio.java
@@ -1,10 +1,12 @@
 package Clases;
+	
+import org.joda.time.LocalDateTime;
 
 public class Servicio {
 	
 	private int millas;
 	private int pago;
-	private String fechaInicio, fechaFin;
+	private LocalDateTime fechaInicio, fechaFin;
 	
 	public Servicio(int pMillas,int pPago) {
 		millas = pMillas;
